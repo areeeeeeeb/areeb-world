389f7d789c251b797bc2bcc8f8e14f596cddd28c
areeeeeeeb <rasulareeb@gmail.com>
2023-08-16 18:35:17 -0300

    extra pleasing eslint

--- pages/success.js
+++ pages/success.js
@@ -7,26 +7,25 @@ import { fetcher, shootFireworks } from '@/lib/utils';
 import { CheckIcon } from '@heroicons/react/outline';
 
 const Success = () => {
-  const {
-    query: { session_id },
-  } = useRouter();
+    const {
+        query: { session_id },
+    } = useRouter();
 
-  useEffect(() => {
-      shootFireworks();
-  }, [data]);
+    useEffect(() => {
+        shootFireworks();
+    }, [data]);
 
-  return (
-    <div className="container xl:max-w-screen-xl mx-auto py-12 px-6 text-center">
-        <div className="py-4 px-8 rounded-md bg-gray-100 max-w-lg mx-auto">
-          <h2 className="text-4xl font-semibold flex flex-col items-center space-x-1">
-            <CheckIcon className="w-12 h-12 flex-shrink-0 text-green-600" />
-            <span>Thanks for your order!</span>
-          </h2>
-          <p className="text-lg mt-3">Check your inbox for the receipt.</p>
+    return (
+        <div className="container xl:max-w-screen-xl mx-auto py-12 px-6 text-center">
+            <div className="py-4 px-8 rounded-md bg-gray-100 max-w-lg mx-auto">
+                <h2 className="text-4xl font-semibold flex flex-col items-center space-x-1">
+                    <CheckIcon className="w-12 h-12 flex-shrink-0 text-green-600" />
+                    <span>Thanks for your order!</span>
+                </h2>
+                <p className="text-lg mt-3">Check your inbox for the receipt.</p>
+            </div>
         </div>
-      )}
-    </div>
-  );
-}
+    );
+};
 
 export default Success;
