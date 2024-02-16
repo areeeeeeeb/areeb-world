cfa36a60cc014c21ce0c1854434443ee262fa171
areeeeeeeb <rasulareeb@gmail.com>
2024-02-16 04:11:38 -0500

    added design.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   webpack5: true,
-};
+  async redirects() {
+    return [
+      {
+        source: '/design',
+        destination: '/Selected Works By Areeb Rasul.pdf',
+        permanent: false
+      },
+    ]
+  },
+}
---
+++ pages/design.js
@@ -0,0 +1,5 @@
+// pages/api/design.js
+
+export default function handler(req, res) {
+    res.redirect('/selected-works-by-areeb.pdf');
+}
